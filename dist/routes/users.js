29c384a2aca7f338d9b5807a0141e0d66ba9bd53
vbisso <valeriabisso19@gmail.com>
2025-05-22 19:33:54 -0600

    get id, put, delete place holders

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -2,5 +2,8 @@
 const usersRoute = require("express").Router();
 const { getAllUsers, addUser } = require("../controllers/users");
 usersRoute.get("/", getAllUsers);
+usersRoute.get("/:id", getAllUsers); //for api-docs purpose, will be updated later
 usersRoute.post("/", addUser);
+usersRoute.put("/", getAllUsers); //for api-docs purpose, will be updated later
+usersRoute.delete("/", getAllUsers); //for api-docs purpose, will be updated later
 module.exports = usersRoute;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,6 +2,9 @@ const usersRoute = require("express").Router();
 const { getAllUsers, addUser } = require("../controllers/users");
 
 usersRoute.get("/", getAllUsers);
+usersRoute.get("/:id", getAllUsers); //for api-docs purpose, will be updated later
 usersRoute.post("/", addUser);
+usersRoute.put("/", getAllUsers); //for api-docs purpose, will be updated later
+usersRoute.delete("/", getAllUsers); //for api-docs purpose, will be updated later
 
 module.exports = usersRoute;
